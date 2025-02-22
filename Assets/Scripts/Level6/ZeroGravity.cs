486a15a9ee30b6abe7d59e77732eb6974081472e
MarkCup <1586827710@qq.com>
2025-02-22 12:06:12 +0800

    第六关收工

--- Assets/Scripts/Level6/ZeroGravity.cs
+++ Assets/Scripts/Level6/ZeroGravity.cs
@@ -8,18 +8,38 @@ public class ZeroGravity : MonoBehaviour
     public AudioClip music;
     public void SetZeroGravity()
     {
+        g = Physics2D.gravity;
         Physics2D.gravity = Vector2.zero;
         rb.drag = 1;
 
         SoundManager.Instance.PlayMusic(music);
 
         enabled=true;
+
+        col.enabled = false;
+        col2.enabled = false;
     }
+
+    public float timmer = 36.5f;
+
+    Vector2 g;
+
     private void Update()
     {
-        if (rb.transform.localScale.x < 2)
+        timmer -= Time.deltaTime;
+        if (timmer < 0)
         {
-            rb.transform.localScale += Vector3.one * Time.deltaTime *(2-rb.transform.localScale.x)*0.1f;
+            col.enabled = true;
+            enabled = false;
+            Physics2D.gravity = g;
+        }
+        if (rb.transform.localScale.x < 3.5f)
+        {
+            rb.transform.localScale += Vector3.one * Time.deltaTime *(3.5f-rb.transform.localScale.x)*0.1f;
         }
     }
+
+    public Collider2D col,col2;
+
+
 }
